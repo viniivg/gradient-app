68b8bbb223487ea659fc606d256234332eb31f89
Vinícius Guedes <viniciusguedes@MacBook-Pro-de-Vinicius-2.local>
2021-12-02 20:31:38 -0300

    Create app

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,8 @@
+import LinearGradient from "react-native-linear-gradient";
+import styled from "styled-components";
+
+export const Container = styled(LinearGradient).attrs({
+    colors: ["#0277BD", "#757575"]
+})`
+   flex: 1;
+`
